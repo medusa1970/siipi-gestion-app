919fecdbf527ddcf96ae6412e0a728ce4026f86f
leoprod2023 <leoprod2023@gmail.com>
2024-06-13 12:42:44 -0400

    desactivado menuListSede

--- src/layouts/sede.vue
+++ src/layouts/sede.vue
@@ -1,12 +1,13 @@
 <template>
-  <layout :menuList="menuListSede" sede :portada-img="PortadaSede">
+  <!-- <layout :menuList="menuListSede" sede :portada-img="PortadaSede">
     <template #slot>
       <slot />
     </template>
-  </layout>
+  </layout> -->
+  <h1>En desarollo</h1>
 </template>
 
 <script setup>
-import { menuListSede } from '@/helpers/menuList';
-import PortadaSede from '@/assets/img/buddha.png';
+// import { menuListSede } from '@/helpers/menuList';
+// import PortadaSede from '@/assets/img/buddha.png';
 </script>

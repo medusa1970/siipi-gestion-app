941c1415ca6470a2ff35175b8809328be3b84333
leoprod2023 <leoprod2023@gmail.com>
2024-06-15 13:22:55 -0400

    escondido los menus sin uso de nuevo

--- src/helpers/menuList.ts
+++ src/helpers/menuList.ts
@@ -11,72 +11,72 @@ export const menuListCathering = [
         label: 'Categorias de productos ',
         to: '/cathering/sede/productos/categorias',
       },
-      {
-        label: 'Proveedores',
-        to: '/cathering',
-      },
-    ],
-  },
-  {
-    icon: 'warehouse',
-    label: 'Almacen',
-    subMenu: [
-      {
-        label: 'Stock ',
-        to: '/cathering/stock',
-      },
-      {
-        label: 'Adquisicion',
-        to: '/cathering/pedidos/recibirProductos/85a4475e446a5885b05739c4',
-      },
-      {
-        label: 'Inventario',
-        to: '/cathering/inventario',
-      },
       // {
-      //   label: 'Despacho',
-      //   to: '/cathering/pedidos/despacharProductos',
+      //   label: 'Proveedores',
+      //   to: '/cathering',
       // },
     ],
   },
-  {
-    icon: 'sell',
-    label: 'Venta',
-    subMenu: [
-      {
-        label: 'Ofertas',
-        to: '/cathering/sede/ofertas',
-      },
-      {
-        label: 'Catalogos de oferta',
-        to: '/cathering/sede/catalogos/75a4475e446a5885b05739c4',
-      },
-    ],
-  },
-  {
-    icon: 'description',
-    label: 'Pedidos',
-    subMenu: [
-      {
-        label: 'ver pedidos',
-        to: '/cathering/pedidos/listaPedidos',
-      },
-    ],
-  },
-  {
-    icon: 'sell',
-    label: 'Areas',
-    subMenu: [
-      {
-        label: 'Panaderia',
-        to: '/cathering',
-      },
-      {
-        label: 'Reposteria',
-        to: '/cathering',
-      },
-    ],
-  },
+  // {
+  //   icon: 'warehouse',
+  //   label: 'Almacen',
+  //   subMenu: [
+  //     {
+  //       label: 'Stock ',
+  //       to: '/cathering/stock',
+  //     },
+  //     {
+  //       label: 'Adquisicion',
+  //       to: '/cathering/pedidos/recibirProductos/85a4475e446a5885b05739c4',
+  //     },
+  //     {
+  //       label: 'Inventario',
+  //       to: '/cathering/inventario',
+  //     },
+  //     // {
+  //     //   label: 'Despacho',
+  //     //   to: '/cathering/pedidos/despacharProductos',
+  //     // },
+  //   ],
+  // },
+  // {
+  //   icon: 'sell',
+  //   label: 'Venta',
+  //   subMenu: [
+  //     {
+  //       label: 'Ofertas',
+  //       to: '/cathering/sede/ofertas',
+  //     },
+  //     {
+  //       label: 'Catalogos de oferta',
+  //       to: '/cathering/sede/catalogos/75a4475e446a5885b05739c4',
+  //     },
+  //   ],
+  // },
+  // {
+  //   icon: 'description',
+  //   label: 'Pedidos',
+  //   subMenu: [
+  //     {
+  //       label: 'ver pedidos',
+  //       to: '/cathering/pedidos/listaPedidos',
+  //     },
+  //   ],
+  // },
+  // {
+  //   icon: 'sell',
+  //   label: 'Areas',
+  //   subMenu: [
+  //     {
+  //       label: 'Panaderia',
+  //       to: '/cathering',
+  //     },
+  //     {
+  //       label: 'Reposteria',
+  //       to: '/cathering',
+  //     },
+  //   ],
+  // },
 
   {
     icon: 'people',
@@ -92,16 +92,16 @@ export const menuListCathering = [
       },
     ],
   },
-  {
-    icon: 'inventory_2',
-    label: 'Gestion',
-    subMenu: [
-      {
-        label: 'Problemas de inventario',
-        to: '/cathering/inventario/listaProblemas',
-      },
-    ],
-  },
+  // {
+  //   icon: 'inventory_2',
+  //   label: 'Gestion',
+  //   subMenu: [
+  //     {
+  //       label: 'Problemas de inventario',
+  //       to: '/cathering/inventario/listaProblemas',
+  //     },
+  //   ],
+  // },
 ];
 
 export const menuListPunto = [

bad17aee16cd3bb2ddbcadd8e5d8de6c76ff2404
willCh <willchcode@gmail.com>
2024-08-01 14:44:58 -0400

    sockets url

--- src/plugins/socket.config.ts
+++ src/plugins/socket.config.ts
@@ -8,11 +8,11 @@ declare module '#app' {
 }
 const debug = false;
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(nuxtApp => {
   const socketUrl =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://siipi-gestion-production-f6a0.up.railway.app';
+      : 'https://api.gestion.sii-pi.com';
   const socket = io(socketUrl, {});
 
   // Manejador para el evento de conexión exitosa
@@ -21,12 +21,12 @@ export default defineNuxtPlugin((nuxtApp) => {
   });
 
   // Manejador para el evento de error de conexión
-  socket.on('connect_error', (error) => {
+  socket.on('connect_error', error => {
     if (debug) console.log('Fallo al conectarse a sockets:', error);
   });
 
   // Agregar un manejador para otros eventos podría ser útil para la depuración
-  socket.on('disconnect', (reason) => {
+  socket.on('disconnect', reason => {
     if (debug) console.log('Desconectado del socket:', reason);
   });
 
